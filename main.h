a0f34c0a12057a31b7fd0dd9ca5816bc03d50dfa
Tu276 <tu276nathan@gmail.com>
2023-06-27 21:49:15 +0300

    print f

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_str(char *c);
+int print_int(int num);
+int print_Str(char *c);
+int print_bin(unsigned int num);
+int print_hex(unsigned int num);
+int print_oct(unsigned int num);
+int print_unt(unsigned int num);
+int print_hexlow(unsigned int num);
+int print_bin(unsigned int num);
+int print_ptr(int num);
+int print_rot13(char *c);
+int print_rev(char *c);
+#endif
