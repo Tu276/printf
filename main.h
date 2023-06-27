221a3fa74ae299f6d9b91f89d8360b381f2dc06a
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-06-27 22:45:15 +0200

    modified

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
+/* Function prototypes */
+int _printf(const char *format, ...);
+
+/*function to print strings */
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_str(char *c);
@@ -15,4 +19,8 @@ int print_bin(unsigned int num);
 int print_ptr(int num);
 int print_rot13(char *c);
 int print_rev(char *c);
+
+/* Function pointer declaration */
+typedef int (*print_func_ptr)(va_list);
+
 #endif
--- printf.c
+++ printf.c
@@ -8,78 +8,75 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int i = 0, count = 0, flag = 0;
+	va_list ap;
+	int i = 0, count = 0, flag = 0;
 
-    if (format)
-    {
-        va_start(ap, format);
-        for (; format[i] != '\0'; i++)
-        {
-            if (!flag)
-            {
-                if (format[i] != '%')
-                    count += _putchar(format[i]);
-                else
-                    flag = 1;
-            }
-            else
-            {
-                switch (format[i])
-                {
-                case 'c':
-                    count += _putchar(va_arg(ap, int));
-                    break;
-                case 's':
-                    count += print_str(va_arg(ap, char *));
-                    break;
-                case '%':
-                    count += _putchar('%');
-                    break;
-                case 'd':
-                    count += print_int(va_arg(ap, int));
-                    break;
-                case 'i':
-                    count += print_int(va_arg(ap, int));
-                    break;
-                case 'b':
-                    count += print_bin(va_arg(ap, unsigned int));
-                    break;
-                case 'u':
-                    count += print_unt(va_arg(ap, unsigned int));
-                    break;
-                case 'o':
-                    count += print_oct(va_arg(ap, unsigned int));
-                    break;
-                case 'x':
-                    count += print_hexlow(va_arg(ap, unsigned int));
-                    break;
-                case 'X':
-                    count += print_hex(va_arg(ap, unsigned int));
-                    break;
-                case 'S':
-                    count += print_Str(va_arg(ap, char *));
-                    break;
-                case 'R':
-                    count += print_rot13(va_arg(ap, char *));
-                    break;
-                case 'r':
-                    count += print_rev(va_arg(ap, char *));
-                    break;
-                case '\0':
-                    return (-1);
-                default:
-                    count += _putchar('%');
-                    count += _putchar(format[i]);
-                }
-                flag = 0;
-            }
-        }
-        va_end(ap);
-    }
-    else
-    {
-        return (-1);
-    }
-    return (count);
+	if (format)
+	{
+		va_start(ap, format);
+		for (; format[i] != '\0'; i++)
+		{
+			if (!flag)
+			{
+				if (format[i] != '%')
+					count += _putchar(format[i]);
+				else
+					flag = 1;
+			}
+			else
+			{
+				int printed = 0;
+				int(*print_func)(va_list) = NULL;
+				switch (format[i])
+				{
+					case 'c':
+						print_func = print_char;
+						break;
+					case 's':
+						print_func = print_str;
+						break;
+					case '%':
+						print_func = print_percent;
+						break;
+					case 'd':
+					case 'i':
+						print_func = print_int;
+						break;
+					case 'b':
+						print_func = print_bin;
+						break;
+					case 'u':
+						print_func = print_unt;
+						break;
+					case 'o':
+						print_func = print_oct;
+						break;
+					case 'x':
+						print_func = print_hexlow;
+						break;
+					case 'X':
+						print_func = print_hex;
+						break;
+					case 'S':
+						print_func = print_Str;
+						break;
+					case 'R':
+						print_func = print_rot13;
+						break;
+					case 'r':
+						print_func = print_rev;
+						break;
+				}
+				if (print_func)
+					count += print_func(ap), printed = 1;
+				if (!printed && format[i])
+					count += _putchar('%'), count += _putchar(format[i]);
+				flag = 0;
+			}
+		}
+		va_end(ap);
+	}
+	else
+		return (-1);
+	return (count);
 }
