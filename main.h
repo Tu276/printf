bebbc32f01d93b1c1076a53d3109b42e26ebfec0
Tu276 <tu276nathan@gmail.com>
2023-06-30 05:28:38 +0300

    print bin and chars

--- main.h
+++ main.h
@@ -1,18 +1,49 @@
 #ifndef PRINTF_H
 #define PRINTF_H
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_str(char *c);
-int print_int(int num);
-int print_Str(char *c);
-int print_bin(unsigned int num);
-int print_hex(unsigned int num);
-int print_oct(unsigned int num);
-int print_unt(unsigned int num);
-int print_hexlow(unsigned int num);
-int print_bin(unsigned int num);
-int print_ptr(int num);
-int print_rot13(char *c);
-int print_rev(char *c);
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
+
+/**
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
+{
+    char type;
+    int (*f)(va_list);
+} format;
 
 #endif
